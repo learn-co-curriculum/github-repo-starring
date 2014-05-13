4bf0ee23e01f2750ecd3333cf422e91933de456f
Joe Burgess <joe@flatironschool.com>
2014-05-13 11:39:58 -0400

    Adding Updated FISGithubAPIClient Methods
      - check if starred
      - star repo
      - unstar repo

--- github-repo-starring/github-repo-starring/FISGithubAPIClient.m
+++ github-repo-starring/github-repo-starring/FISGithubAPIClient.m
@@ -26,4 +26,55 @@ NSString *const GITHUB_API_URL=@"https://api.github.com";
     }];
 }
 
++(void)checkIfRepoIsStarredWithFullName:(NSString *)fullName CompletionBlock:(void (^)(BOOL))completionBlock
+{
+    NSString *url = [NSString stringWithFormat:@"%@/user/starred/%@?client_id=%@&client_secret=%@",GITHUB_API_URL,fullName, GITHUB_CLIENT_ID,GITHUB_CLIENT_SECRET];
+    AFHTTPSessionManager *manager = [[AFHTTPSessionManager alloc] init];
+    AFHTTPRequestSerializer *serializer = [[AFHTTPRequestSerializer alloc] init];
+    [serializer setAuthorizationHeaderFieldWithUsername:GITHUB_ACCESS_TOKEN password:@""];
+    manager.requestSerializer = serializer;
+
+    [manager GET:url parameters:nil success:^(NSURLSessionDataTask *task, id responseObject) {
+        NSHTTPURLResponse *response = (NSHTTPURLResponse *)task.response;
+        if (response.statusCode == 204 ) {
+            completionBlock(YES);
+        }
+    } failure:^(NSURLSessionDataTask *task, NSError *error) {
+        NSHTTPURLResponse *response = (NSHTTPURLResponse *)task.response;
+        if (response.statusCode == 404 ) {
+            completionBlock(NO);
+        }
+    }];
+}
+
++(void)starRepoWithFullName:(NSString *)fullName CompletionBlock:(void (^)(void))completionBlock
+{
+    NSString *url = [NSString stringWithFormat:@"%@/user/starred/%@?client_id=%@&client_secret=%@",GITHUB_API_URL,fullName, GITHUB_CLIENT_ID,GITHUB_CLIENT_SECRET];
+    AFHTTPSessionManager *manager = [[AFHTTPSessionManager alloc] init];
+    AFHTTPRequestSerializer *serializer = [[AFHTTPRequestSerializer alloc] init];
+    [serializer setAuthorizationHeaderFieldWithUsername:GITHUB_ACCESS_TOKEN password:@""];
+    manager.requestSerializer = serializer;
+
+    [manager PUT:url parameters:nil success:^(NSURLSessionDataTask *task, id responseObject) {
+        completionBlock();
+    } failure:^(NSURLSessionDataTask *task, NSError *error) {
+        NSLog(@"FAIL:%@",error.localizedDescription);
+    }];
+}
+
++(void)unstarRepoWithFullName:(NSString *)fullName CompletionBlock:(void (^)(void))completionBlock
+{
+    NSString *url = [NSString stringWithFormat:@"%@/user/starred/%@?client_id=%@&client_secret=%@",GITHUB_API_URL,fullName, GITHUB_CLIENT_ID,GITHUB_CLIENT_SECRET];
+    AFHTTPSessionManager *manager = [[AFHTTPSessionManager alloc] init];
+    AFHTTPRequestSerializer *serializer = [[AFHTTPRequestSerializer alloc] init];
+    [serializer setAuthorizationHeaderFieldWithUsername:GITHUB_ACCESS_TOKEN password:@""];
+    manager.requestSerializer = serializer;
+
+    [manager DELETE:url parameters:nil success:^(NSURLSessionDataTask *task, id responseObject) {
+        completionBlock();
+    } failure:^(NSURLSessionDataTask *task, NSError *error) {
+        NSLog(@"FAIL:%@",error.localizedDescription);
+    }];
+}
+
 @end
